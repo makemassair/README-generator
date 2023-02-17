095ff04217ef555bf95b24e20a913b95e1563fe8
Michael Samaras <mike@makemassair.co.uk>
2023-02-17 19:10:11 +0000

    Working questions + promot

--- index.js
+++ index.js
@@ -1,73 +1,46 @@
+// declaring dependencies + variables 
+const inquirer = require("inquirer"); 
 const fs = require("fs");
 const path = require('path');
-const inquirer = require("inquirer"); 
 const generateMarkdown = require("./utils/generateMarkdown");
+const questions = require("./utils/questions").questions;
+const { log } = require("console");
 
-// array of questions for user
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please enter a brief description of your project'
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'What are the installation instructions for your project?'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'What is this project used for?'
-    },
-    {
-        type: 'input',
-        name: 'license',
-        message: 'Choose the approriate license for this project',
-        choices: [
-            'Apache',
-            'GNU',
-            'MIT',
-            'BSD 2-Clause',
-            'BSD 3-Clause',
-            'Boost',
-            'Creative Commons',
-            'Eclipse',
-            'Mozilla',
-            'The Unlicense'
-        ]
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Who, if any, are the contributors to this project?'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Is a test included?'
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: 'What do you do if there an issue?'
-    }
-];
+// run question function
+async function runQuery() {
+    return inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+        return answers
+    })
+    .catch((error) => {
+        console.log(error);
+
+    })
+}
+
+runQuery();
+
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, questions) {
+//     return inquirer.prompt(data)
+//     .then((answers) => {
+//         console.log(answers);
+//         return answers
+//     })
+//     .catch((error) => {
+//         console.log(error);
 
-// function to initialize program
-function init() {
+//     })
+// }
 
-}
 
-// function call to initialize program
-init();
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
  
---
+++ utils/questions.js
@@ -0,0 +1,63 @@
+// array of questions for user
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a brief description of your project'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions for your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is this project used for?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose the approriate license for this project',
+        choices: [
+            'Apache 2.0 License',
+            'GNU GPL v3',
+            'The MIT License',
+            'ISC License (ISC)',
+            'BSD 2-Clause License',
+            'BSD 3-Clause License',
+            'Boost Software License 1.0',
+            'Creative Commons CC0',
+            'Eclipse Public License 1.0',
+            'Mozilla Public License 2.0',
+            'The Unlicense'
+        ]
+        // filter(val) {
+        //      return val.toLowerCase(); 
+        // }
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Who, if any, are the contributors to this project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Is a test included?'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'What do you do if there an issue?'
+    }
+];
+
+module.exports = {
+    questions: questions,
+  };
