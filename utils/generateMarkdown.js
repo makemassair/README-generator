cff834be18d44000bff8ab7d6a02a01dcec04694
Michael Samaras <mike@makemassair.co.uk>
2023-02-22 14:02:40 +0000

    Added return strucutre to generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,38 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `
+  # ${answers.title}
+
+  ## Table of Contents
+  - [Project Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+
+  ## Description
+  ${answers.description}
+
+  ## Installation
+  ${answers.installation}
+
+  ## Usage
+  ${answers.usage}
+
+  ## License
+  ${answers.license}
+
+  ## Contributing
+  ${answers.contributing}
+
+  ## Tests
+  ${answers.tests}
+
+  ## Questions
+  ${answers.questions}
+
 
 `;
 }
